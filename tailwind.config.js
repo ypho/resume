8829aaf2872d895891c4df9630a58671b07fcd33
Ivo Geersen <ivo@ypho.nl>
2022-01-13 21:41:49 +0100

    Added dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  darkMode: 'class',
   content: ["./resources/index.html"],
   theme: {
     extend: {
       colors: {
         teal: colors.teal,
         cyan: colors.cyan,
+        gray: colors.zinc,
         logo_coffee: '#FFDD00',
         logo_linkedin: '#0A66C2',
         logo_github: '#181717',
       },
     },
+    nightwind: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/aspect-ratio'),
+    require('nightwind')
   ],
 }
